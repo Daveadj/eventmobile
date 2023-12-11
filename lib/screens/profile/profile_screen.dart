df3ef63da0fe6a724ae00d3ac47cb2a5c01691e5
Daveadj <ovoadjerese@gmail.com>
2023-12-11 01:29:10 +0100

    profileScreen

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -1,16 +1,186 @@
 import 'package:flutter/material.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
 
   @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  bool isSelected = true;
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
         children: [
-          
+          Stack(
+            clipBehavior: Clip.none,
+            children: [
+              Container(
+                height: 200,
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(30),
+                    bottomRight: Radius.circular(
+                      30,
+                    ),
+                  ),
+                ),
+              ),
+              const Positioned(
+                bottom: -80,
+                left: 0,
+                right: 0,
+                child: Center(
+                  child: Column(
+                    children: [
+                      CircleAvatar(
+                        radius: 50,
+                        backgroundColor: Colors.black,
+                        // You can customize the avatar with an image or initials
+                        child: Icon(
+                          Icons.person,
+                          size: 60,
+                          color: Colors.blue,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 7,
+                      ),
+                      Text(
+                        'PostMalone',
+                        style: TextStyle(
+                          fontFamily: 'Lato',
+                          fontSize: 20,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 110,
+          ),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              FollowColumn(
+                number: '99',
+                title: 'Events',
+              ),
+              FollowColumn(
+                number: '12.5M',
+                title: 'Followers',
+              ),
+              FollowColumn(
+                number: '12',
+                title: 'Following',
+              )
+            ],
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              PostAndEventToggleButton(
+                isSelected: isSelected,
+                onPressed: () {
+                  setState(() {
+                    isSelected = true;
+                  });
+                },
+                title: 'Post',
+              ),
+              PostAndEventToggleButton(
+                isSelected: !isSelected,
+                onPressed: () {
+                  setState(() {
+                    isSelected = false;
+                  });
+                },
+                title: 'Events',
+              )
+            ],
+          )
         ],
       ),
+    );
+  }
+}
+
+class PostAndEventToggleButton extends StatelessWidget {
+  const PostAndEventToggleButton({
+    super.key,
+    required this.onPressed,
+    required this.title,
+    required this.isSelected,
+  });
+
+  final Function() onPressed;
+  final String title;
+  final bool isSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 50,
+      width: 150,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          elevation: 0,
+          backgroundColor:
+              isSelected ? const Color.fromARGB(255, 6, 86, 151) : Colors.white,
+        ),
+        child: Text(
+          title,
+          style: TextStyle(
+            fontFamily: 'lato',
+            fontSize: 17,
+            color: isSelected ? Colors.white : Colors.black,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class FollowColumn extends StatelessWidget {
+  const FollowColumn({
+    super.key,
+    required this.number,
+    required this.title,
+  });
+  final String number;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          number,
+          style: const TextStyle(
+            fontFamily: 'Poppins',
+            fontWeight: FontWeight.bold,
+            fontSize: 25,
+          ),
+        ),
+        Text(
+          title,
+          style: const TextStyle(
+            fontFamily: 'Lato',
+            fontSize: 15,
+          ),
+        ),
+      ],
     );
   }
 }
