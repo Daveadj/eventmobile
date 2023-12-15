61b5b2b2548ed9069ce5ab8ae609e8a6ce57c1d6
Daveadj <ovoadjerese@gmail.com>
2023-12-15 22:41:37 +0100

    event details

--- lib/screens/home/event_details_screen.dart
+++ lib/screens/home/event_details_screen.dart
@@ -36,129 +36,142 @@ class _EventDetailsState extends State<EventDetails> {
                 },
                 child: const Icon(
                   Icons.arrow_back,
+                  size: 35,
                 ),
               ),
-              Container(
-                height: 320,
-                width: double.infinity,
-                decoration: const BoxDecoration(
-                  color: Colors.black87,
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(
-                      10,
-                    ),
-                  ),
-                ),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(10),
-                  child: CachedNetworkImage(
-                    imageUrl: widget.event.image,
-                    width: double.infinity,
-                    height: 320,
-                    fit: BoxFit.cover,
-                    placeholder: (context, url) => Shimmer.fromColors(
-                      baseColor: Colors.grey[300]!,
-                      highlightColor: Colors.grey[100]!,
-                      child: Container(
-                        color: Colors.white, // Shimmer effect color
+              Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: Container(
+                  height: 320,
+                  width: double.infinity,
+                  decoration: const BoxDecoration(
+                    color: Colors.black87,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(
+                        10,
                       ),
                     ),
-                    errorWidget: (context, url, error) =>
-                        const Icon(Icons.error),
                   ),
-                ),
-              ),
-              Text(
-                widget.event.description,
-                style: const TextStyle(
-                  fontFamily: 'Poppins',
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Container(
-                    width: 30,
-                    height: 30,
-                    clipBehavior: Clip.antiAlias,
-                    decoration: const BoxDecoration(
-                      shape: BoxShape.circle,
-                    ),
-                    child: Image.network(
-                      'https://picsum.photos/seed/468/600',
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: CachedNetworkImage(
+                      imageUrl: widget.event.image,
+                      width: double.infinity,
+                      height: 320,
                       fit: BoxFit.cover,
+                      placeholder: (context, url) => Shimmer.fromColors(
+                        baseColor: Colors.grey[300]!,
+                        highlightColor: Colors.grey[100]!,
+                        child: Container(
+                          color: Colors.white, // Shimmer effect color
+                        ),
+                      ),
+                      errorWidget: (context, url, error) =>
+                          const Icon(Icons.error),
                     ),
                   ),
-                  const Text(
-                    'PostMalone',
-                    style: TextStyle(
-                      fontFamily: 'Poppins',
-                      fontWeight: FontWeight.bold,
-                      fontSize: 15,
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 8,
-                  ),
-                  Text(
-                    formatDateTime(widget.event.startTime),
-                    style: const TextStyle(
-                      fontFamily: 'Lato',
-                      fontSize: 15,
-                    ),
-                  ),
-                  const Spacer(),
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.message,
-                    ),
-                  )
-                ],
+                ),
               ),
-              const Text(
-                'Post Malone will at the hmv Underground at 333 Yonge Street on March 24, 2015 at 5:00 PM for an exclusive FAN MEET & GREET. Space is limited to the first 300 fans on a first come first served basis (as per the event protocol)',
-                style: TextStyle(
-                  fontFamily: 'Lato',
-                  fontSize: 15,
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8),
+                child: Row(
+                  children: [
+                    Text(
+                      widget.event.description,
+                      style: const TextStyle(
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                    const Spacer(),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.message,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Container(
+                      width: 30,
+                      height: 30,
+                      clipBehavior: Clip.antiAlias,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                      ),
+                      child: Image.network(
+                        'https://picsum.photos/seed/468/600',
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 3,
+                    ),
+                    const Text(
+                      'PostMalone',
+                      style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Text(
+                      formatDateTime(widget.event.startTime),
+                      style: const TextStyle(
+                        fontFamily: 'Lato',
+                        fontSize: 15,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  'Post Malone will at the hmv Underground at 333 Yonge Street on March 24, 2015 at 5:00 PM for an exclusive FAN MEET & GREET. Space is limited to the first 300 fans on a first come first served basis (as per the event protocol)',
+                  style: TextStyle(
+                    fontFamily: 'Lato',
+                    fontSize: 15,
+                  ),
                 ),
               ),
               const SizedBox(
-                height: 20,
+                height: 5,
               ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Center(
-                  child: Container(
-                    height: 45,
-                    width: 300,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(25.0),
+              Center(
+                child: Container(
+                  height: 45,
+                  width: 300,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(25.0),
+                  ),
+                  child: const TabBar(
+                    labelColor: Colors.black,
+                    unselectedLabelColor: Colors.black,
+                    indicatorSize: TabBarIndicatorSize.tab,
+                    labelStyle: TextStyle(
+                      fontFamily: 'lato',
+                      fontSize: 17,
                     ),
-                    child: TabBar(
-                      labelColor: Colors.white,
-                      unselectedLabelColor: Colors.black,
-                      indicatorSize: TabBarIndicatorSize.tab,
-                      labelStyle: const TextStyle(
-                        fontFamily: 'lato',
-                        fontSize: 17,
+                    tabs: [
+                      Tab(
+                        text: 'Location',
                       ),
-                      indicator: BoxDecoration(
-                        color: Colors.blue, // Set the indicator color
-                        borderRadius: BorderRadius.circular(25.0),
-                      ),
-                      tabs: const [
-                        Tab(
-                          text: 'Location',
-                        ),
-                        Tab(
-                          text: 'Pricing',
-                        )
-                      ],
-                    ),
+                      Tab(
+                        text: 'Pricing',
+                      )
+                    ],
                   ),
                 ),
               ),
