0547c50b53623cd552c6541f0240ad56c106f64c
Daveadj <ovoadjerese@gmail.com>
2023-12-17 02:39:27 +0100

    add onTasp

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -157,7 +157,20 @@ class _HomeScreenState extends State<HomeScreen> {
                       return Padding(
                         padding: const EdgeInsets.symmetric(
                             vertical: 8, horizontal: 14),
-                        child: SmallEventContainer(event: events[index]),
+                        child: GestureDetector(
+                          onTap: () {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (ctx) => EventDetails(
+                                  event: events[index],
+                                ),
+                              ),
+                            );
+                          },
+                          child: SmallEventContainer(
+                            event: events[index],
+                          ),
+                        ),
                       );
                     },
                   ),
